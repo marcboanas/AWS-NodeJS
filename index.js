b27d6c589534f3fff4cc9a4582a02a6ae77037e2
marcboanas <marcjohnboanas@gmail.com>
2017-05-20 14:47:09 +0100

    Serving static html

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+// const express = require('express')
+// const app = express()
+// app.get('/', (req, res) => {
+// 	res.send('HEY!')
+// })
+
+// app.listen(3000, () => console.log('Server running on port 3000'))
+
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => {
-	res.send('HEY!')
-})
-
-app.listen(3000, () => console.log('Server running on port 3000'))
+app.use(express.static('public'))
+app.listen(3000)
