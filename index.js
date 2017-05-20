7f9c5b98327d1cf819e6101047b11725c558e042
marcboanas <marcjohnboanas@gmail.com>
2017-05-20 14:41:48 +0100

    statis html file

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require('express')
 const app = express()
-app.use(express.static('public'))
+app.get('/', (req, res) => {
+	res.send('HEY!')
+})
+
 app.listen(3000, () => console.log('Server running on port 3000'))
